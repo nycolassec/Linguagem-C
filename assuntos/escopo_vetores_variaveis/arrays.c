57ef739e2312a0bbb6efc8da691fa5d2068aeecb
NycolasSec <nycolasonrails@gmail.com>
2024-11-01 19:54:28 -0300

    c - arrays

---
+++ assuntos/escopo_vetores_variaveis/arrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM 5
+
+int main() {
+    char tamanhos[TAM] = {"P", "M", "G", "A", "Q"};
+    int distancias[TAM] = {45, 76, 12, 75, 24};
+    float precos[TAM] = {12.50, 50.50, 99.90, 14.50, 70.25}
+
+    // Modificando o valor com um valor do usuário
+    printf("Escolha o primeiro valor dos arrays:\n");
+
+    for (int i = 0, ) {}
+
+    return 0;
+}
--- exercicios/switch_escolha_operacao.c
+++ exercicios/switch_escolha_operacao.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-int main(void){
-    
-
+int main(void) {
     return 0;
 };
