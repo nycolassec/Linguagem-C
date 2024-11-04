1a1ddb3a25b2ce730372cde37e0133d4e9e79c70
Nycolas Ramos dos Sants <nycolasonrails@gmail.com>
2024-11-03 23:48:00 -0300

    Create vetores.c

---
+++ assuntos/escopo_vetores_variaveis/vetores.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("========== Jogo da velha ==========\n");
+    
+    char tab[3][3] = {{' ',' ', ' '}, {' ',' ', ' '} {' ',' ', ' '}};
+
+
+
+    printf("========== Jogo da velha ==========\n");
+    return 0;
+}
