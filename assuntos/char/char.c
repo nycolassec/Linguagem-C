0c76febff97085eabfa9238cf1ee21b5f7db7738
Nyc <nyc@MacBook-de-Nyc.local>
2025-03-04 20:24:05 -0300

    char em C

---
+++ assuntos/char/char.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    char
+        letter_01 = 65,
+        letter_02 = 66,
+        letter_03 = 67;
+
+    printf("Primeira letra - %c \nSegunda letra - %d \nTerceira letra - %c", letter_01, letter_02, letter_03);
+
+    return 0;
+}
